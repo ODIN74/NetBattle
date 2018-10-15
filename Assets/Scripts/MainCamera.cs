7b85364d91f8cdc3794ea029bf0cf859306708f8
ODIN74 <fantompus@gmail.com>
2018-10-16 01:40:25 +0500

    Сделан игрок, его контроллер (всё кроме стрельбы и смерти), анимация, частично UI

--- Assets/Scripts/MainCamera.cs
+++ Assets/Scripts/MainCamera.cs
@@ -16,11 +16,19 @@ public class MainCamera : MonoBehaviour
     [Range(1f, 5f)]
     private float _offsetCameraY = 1f;
 
+    [SerializeField]
+    private float _maxAngleOfCameraDiviation = 10f;
+
+    [SerializeField]
+    private float _sensitivityY = 1f;
+
     private Transform _playerTransform;
 
     private Transform _cameraTransform;
 
-	private void Start ()
+    private float _currentY;
+
+    private void Start ()
     {
         _playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
         _cameraTransform = transform;
@@ -28,15 +36,17 @@ public class MainCamera : MonoBehaviour
 
     private void Update()
     {
+        _currentY += Input.GetAxis("Mouse Y");
     }
 
      private void LateUpdate ()
     {
         var dir = new Vector3(0, _distanceToPlayerY, -_distanceToPlayerZ);
-        var rot = Quaternion.Euler(0f, _playerTransform.rotation.eulerAngles.y, 0f);
+        var rot = Quaternion.Euler(-Mathf.Clamp(_currentY * _sensitivityY, -_maxAngleOfCameraDiviation, _maxAngleOfCameraDiviation), _playerTransform.rotation.eulerAngles.y, 0f);
 
         _cameraTransform.position = (_playerTransform.position + rot * dir);
 
         _cameraTransform.LookAt(_playerTransform.position + Vector3.up * _offsetCameraY);
+        
     }
 }
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+
+{
+    [SerializeField]
+    private float sensitivityX = 1f;
+
+
+
+    [SerializeField]
+    private float speed =5f;
+
+    private Transform _playerTransform;
+    private Animator _playerAnim;
+
+    private float currentX;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        _playerTransform = transform;
+        _playerAnim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        currentX += Input.GetAxis("Mouse X");
+
+        _playerTransform.rotation = Quaternion.Euler(0f, currentX * sensitivityX, 0);
+
+        if (Input.GetAxis("Vertical") > 0)
+        {
+            _playerTransform.Translate(Vector3.forward * speed * Time.deltaTime);
+            if (_playerAnim)
+            {
+                _playerAnim.ResetTrigger("Idle");
+                _playerAnim.ResetTrigger("Backward");
+                _playerAnim.ResetTrigger("ToLeft");
+                _playerAnim.ResetTrigger("ToRight");
+                _playerAnim.SetTrigger("Forward");
+            }
+        }
+        else if (Input.GetAxis("Vertical") < 0)
+        {
+            _playerTransform.Translate(Vector3.back * speed * Time.deltaTime);
+            if (_playerAnim)
+            {
+                _playerAnim.ResetTrigger("Idle");
+                _playerAnim.ResetTrigger("Forward");
+                _playerAnim.ResetTrigger("ToLeft");
+                _playerAnim.ResetTrigger("ToRight");
+                _playerAnim.SetTrigger("Backward");
+            }
+                
+        }
+        else if (Input.GetAxis("Horizontal") > 0)
+        {
+            _playerTransform.Translate(Vector3.right * speed * Time.deltaTime);
+            if (_playerAnim)
+            {
+                _playerAnim.ResetTrigger("Idle");
+                _playerAnim.ResetTrigger("Forward");
+                _playerAnim.ResetTrigger("ToLeft");
+                _playerAnim.ResetTrigger("Backward");
+                _playerAnim.SetTrigger("ToRight");
+            }
+        }
+        else if (Input.GetAxis("Horizontal") < 0)
+        {
+            _playerTransform.Translate(Vector3.left * speed * Time.deltaTime);
+            if (_playerAnim)
+            {
+                _playerAnim.ResetTrigger("Idle");
+                _playerAnim.ResetTrigger("Forward");
+                _playerAnim.ResetTrigger("ToRight");
+                _playerAnim.ResetTrigger("Backward");
+                _playerAnim.SetTrigger("ToLeft");
+            }
+        }
+        else
+        {
+            if (_playerAnim)
+            {
+                _playerAnim.ResetTrigger("ToRight");
+                _playerAnim.ResetTrigger("Forward");
+                _playerAnim.ResetTrigger("ToLeft");
+                _playerAnim.ResetTrigger("Backward");
+                _playerAnim.SetTrigger("Idle");
+            }
+
+        }
+            
+    }
+}
