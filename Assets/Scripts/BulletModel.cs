e15cab7dc7535c809952826276df9cec3ac569d7
ODIN74 <fantompus@gmail.com>
2018-10-16 17:53:26 +0500

    Добавлена стрельба 2

---
+++ Assets/Scripts/BulletModel.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletModel : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed;
+
+    [SerializeField]
+    private float _bulletLifetime = 10f;
+
+    [SerializeField]
+    private float _damage = 10f;
+
+    private Transform _bulletTransform;
+
+    private bool _isHitted;
+
+    private Vector3 _targetPosition;
+
+    private ParticleSystem _particle;
+
+    public void Initialize(Vector3 targetPosition)
+    {
+
+        //_bulletTransform.position = _firePoint.position;
+        //_bulletTransform.rotation = _firePoint.rotation;
+        //Instantiate(gameObject, _bulletTransform.position, _bulletTransform.rotation);
+        _particle = GetComponent<ParticleSystem>();
+        _targetPosition = targetPosition;
+        _isHitted = false;
+        StartCoroutine(BulletDestroyer(_bulletLifetime));
+    }
+
+    private void FixedUpdate()
+    {
+
+        if (_isHitted)
+            return;
+
+        Vector3 finalPos = transform.position + (_targetPosition - transform.position).normalized * _speed * Time.fixedDeltaTime;
+        RaycastHit hit;
+        if (Physics.Linecast(transform.position, finalPos, out hit))
+        {
+            if ((1 << hit.collider.gameObject.layer) != LayerMask.NameToLayer("Player"))
+            {
+                _isHitted = true;
+                transform.position = hit.point;
+
+                Destroy(gameObject);
+            }
+
+            IDamagable d = hit.collider.GetComponent<IDamagable>();
+            if (d != null)
+            {
+                transform.position = hit.point;
+                d.Damage(_damage);
+                Destroy(gameObject);
+            }
+        }
+        else
+        {
+            transform.position = finalPos;
+        }
+    }
+
+    private IEnumerator BulletDestroyer(float bulletLifetime)
+    {
+        yield return new WaitForSeconds(bulletLifetime);
+        Destroy(gameObject);
+        StopCoroutine("BulletDestroyer");
+    }
+}
---
+++ Assets/Scripts/PlayerModel.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerModel : MonoBehaviour, IDamagable
+{
+    [SerializeField]
+    private float _maxHealth = 100f;
+
+    [SerializeField]
+    private float _destroyDelay = 3f;
+
+    [SerializeField]
+    private float _offsetY = 80f;
+
+    private Image _fillHealthBar;
+
+    private Animator _anim;
+
+    [HideInInspector]
+    public float GetHealth { get; private set; }
+
+    [HideInInspector]
+    public Vector3 TargetPoint { get; private set; }
+
+    private Image _aim;
+
+    private Camera _mainCamera;
+
+    private Color _normalAimColor;
+
+    void Start ()
+    {
+        _mainCamera = Camera.main;
+        _anim = GetComponent<Animator>();
+        _aim = GameObject.FindWithTag("Aim").GetComponent<Image>();
+        _normalAimColor = _aim.color;
+        GetHealth = _maxHealth;
+        var _images = GetComponentsInChildren<Image>();
+        foreach(var obj in _images)
+        {
+            if(obj.type != Image.Type.Filled)
+            {
+                continue;
+            }
+            else
+            {
+                _fillHealthBar = obj;
+                break;
+            }
+        }
+        if (_fillHealthBar)
+            _fillHealthBar.fillAmount = GetHealth / _maxHealth;
+	}
+
+    public void FixedUpdate()
+    {
+        var raycastStartPoint = new Vector3(_mainCamera.pixelWidth / 2 + _offsetY, _mainCamera.pixelHeight / 2, 2f);
+        var ray = _mainCamera.ScreenPointToRay(raycastStartPoint);
+        RaycastHit hit;
+        Physics.Raycast(ray, out hit);
+        if (hit.collider != null && (1 << hit.collider.gameObject.layer) == LayerMask.NameToLayer("Player"))
+            _aim.color = Color.red;
+        else if (!_aim.color.Equals(_normalAimColor))
+            _aim.color = _normalAimColor;            
+        TargetPoint = hit.point;
+    }
+
+    public void Damage (float damage)
+    {
+        if (GetHealth <= 0)
+            return;
+
+        GetHealth -= damage;
+        if(_fillHealthBar)
+            _fillHealthBar.fillAmount = GetHealth / _maxHealth;
+
+        if (GetHealth <= 0)
+            Death();
+    }
+
+    public void Death()
+    {
+        if (_anim)
+        {
+            _anim.ResetTrigger("Forward");
+            _anim.ResetTrigger("Backward");
+            _anim.ResetTrigger("ToRight");
+            _anim.ResetTrigger("ToLeft");
+            _anim.ResetTrigger("Idle");
+            _anim.SetTrigger("Die");
+            Destroy(gameObject, _destroyDelay);
+        }
+        else
+            Destroy(gameObject);
+    }
+}
